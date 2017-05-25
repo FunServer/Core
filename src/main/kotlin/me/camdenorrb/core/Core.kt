41a84fbdb3b97f273ddc9b7cd675a951e6fddccf
camdenorrb <camdenorrb@me.com>
2017-05-25 04:52:51 -0500

    Init

---
+++ src/main/kotlin/me/camdenorrb/core/Core.kt
@@ -0,0 +1,17 @@
+package me.camdenorrb.core
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * Created by camdenorrb on 5/25/17.
+ */
+
+class Core : JavaPlugin() {
+
+	override fun onLoad() = Unit
+
+	override fun onEnable() = Unit
+
+	override fun onDisable() = Unit
+
+}
