163b64891b6607c0aa904abe35db1e6c51d007f1
camdenorrb <camdenorrb@me.com>
2017-05-25 05:42:45 -0500

    Started work on the following issues: #2 #9 #4
    + Got the Command Template done.
    + Made FunRank Class for a placeholder, for a soon to be Enum.
    + Made a Account placeholder that currently only holds the Player object in a transient state.

---
+++ src/main/kotlin/me/camdenorrb/core/account/Account.kt
@@ -0,0 +1,9 @@
+package me.camdenorrb.core.account
+
+import org.bukkit.entity.Player
+
+/**
+ * Created by camdenorrb on 5/25/17.
+ */
+
+data class Account(@Transient val player: Player)
---
+++ src/main/kotlin/me/camdenorrb/core/rank/FunRank.kt
@@ -0,0 +1,8 @@
+package me.camdenorrb.core.rank
+
+/**
+ * Created by camdenorrb on 5/25/17.
+ */
+
+class FunRank {
+}
